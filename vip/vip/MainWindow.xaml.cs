9d125eb1acf07fc6780cd407313a5d42e2b4a2b7
海明 张 <zhang_haiming@hotmail.com>
2018-06-09 20:09:46 +0800

    20180609

---
+++ vip/vip/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vip
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateDB();
+        }
+        //---创建数据库
+        static void CreateDB()
+        {
+            string path = "db\\vip.sqlite";
+            SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+            cn.Open();
+            cn.Close();
+        }
+}
+}
