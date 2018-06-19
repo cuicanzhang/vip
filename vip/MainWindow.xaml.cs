7a7257c2f95fb326b748a7cd95c92c5832a371f5
CuiCan_zhang <CuiCan_zhang@qq.com>
2018-06-19 10:15:10 +0800

    20180619

--- vip/MainWindow.xaml.cs
+++ vip/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -15,28 +18,76 @@ namespace vip
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 该函数设置由不同线程产生的窗口的显示状态。
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <param name="cmdShow">指定窗口如何显示。查看允许值列表，请查阅ShowWlndow函数的说明部分。</param>
+        /// <returns>如果函数原来可见，返回值为非零；如果函数原来被隐藏，返回值为零。</returns>
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        /// <summary>
+        /// 该函数将创建指定窗口的线程设置到前台，并且激活该窗口。键盘输入转向该窗口，并为用户改各种可视的记号。系统给创建前台窗口的线程分配的权限稍高于其他线程。
+        /// </summary>
+        /// <param name="hWnd">将被激活并被调入前台的窗口句柄。</param>
+        /// <returns>如果窗口设入了前台，返回值为非零；如果窗口未被设入前台，返回值为零。</returns>
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        private const int WS_SHOWNORMAL = 1;
+        /// <summary>
+        /// 获取正在运行的实例，没有运行的实例返回null;
+        /// </summary>
+        private static Process RuningInstance()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            Process[] Processes = Process.GetProcessesByName(currentProcess.ProcessName);
+            foreach (Process process in Processes)
+            {
+                if (process.Id != currentProcess.Id)
+                {
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == currentProcess.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 显示已运行的程序。
+        /// </summary>
+        public static void HandleRunningInstance(Process instance)
+        {
+            ShowWindowAsync(instance.MainWindowHandle, WS_SHOWNORMAL); //显示，可以注释掉
+            SetForegroundWindow(instance.MainWindowHandle);            //放到前端
+        }
         public MainWindow()
         {
-            InitializeComponent();
-            //数据库初始化
-            conn.Init();
-            /*
-            StackPanel pannel = new StackPanel();
-            Button button=new Button() { Content="button1"};
-            TextBlock textblock = new TextBlock() { Text = "TextBlock1" };
-            pannel.Children.Add(button);
-            pannel.Children.Add(textblock);
-            TabItem item = as TabItem;
-            */
-
-            LoginWindow login = new LoginWindow();
-            login.ShowDialog();
-            if(login.DialogResult!=Convert.ToBoolean(1))
+            Process process = RuningInstance();
+            if (process == null)
             {
-                this.Close();
+                InitializeComponent();
+                //数据库初始化
+                conn.Init();
+                LoginWindow login = new LoginWindow();
+                login.ShowDialog();
+                if (login.DialogResult != Convert.ToBoolean(1))
+                {
+                    this.Close();
+                }
+                Tools.birthdayDic();
+                dataReLoad();
+
             }
-            Tools.birthdayDic();
-            dataReLoad();
+            else
+            {
+                //MessageBox.Show("应用程序已经在运行中。。。");
+                HandleRunningInstance(process);
+                //System.Threading.Thread.Sleep(1000);
+                System.Environment.Exit(1);
+            }
+
         }
         public void dataReLoad()
         {
